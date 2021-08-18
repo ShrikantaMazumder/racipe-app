8a7f49faba3f18ee20a58b69d3262618e0a662e3
Shrikanta Mazumder <shrikantamazumder@gmail.com>
2021-08-18 20:07:36 +0600

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Shrikanta Mazumder on 18/8/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
