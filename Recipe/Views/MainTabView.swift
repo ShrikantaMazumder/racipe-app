c7245cf7f0c8973fd9ed67568541827ab8f2134b
Shrikanta Mazumder <shrikantamazumder@gmail.com>
2021-08-19 10:01:58 +0600

    added tabview

---
+++ Recipe/Views/MainTabView.swift
@@ -0,0 +1,31 @@
+//
+//  MainTabView.swift
+//  Recipe
+//
+//  Created by Shrikanta Mazumder on 19/8/21.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured Tab")
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("Featured")
+                }
+            RecipeListView()
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Recipes")
+                }
+        }
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
